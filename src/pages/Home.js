cd64545efb114d68c8aef88ae47ce4d66a2a6493
cbabuccuoglu <cagribabuccuoglu@gmail.com>
2020-11-07 21:08:17 +0300

    Initial config

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {
+    Typography,
+} from '@material-ui/core';
+
+export default () => (
+    <Typography variant="h4">
+        Welcome Home!
+    </Typography>
+);
